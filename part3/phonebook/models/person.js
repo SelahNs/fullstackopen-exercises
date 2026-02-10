cad81a752d4543e6727b9c21d22c76d2f1ecb6ae
Selehadin Nasir <nasir.selehadin@gmail.com>
2026-02-10 10:01:19 +0300

    feat: integrated the backend with the mongodb

---
+++ part3/phonebook/models/person.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose')
+
+// if (process.argv.length < 3) {
+//     console.log('Give the password')
+//     process.exit(1);
+// }
+
+const uri = process.env.PHONEBOOK_URI
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(uri, {family: 4}).then(result => {
+    console.log('connected to monogodb')
+})
+.catch(error => {
+    console.group('error connecting to MongoDB:', error.message)
+})
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+}) 
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
+
+
+
+
+
+
+// if(process.argv.length === 3) {
+//     Person.find({}).then(result => {
+//         console.log("phonebook:");
+//         result.forEach(person => console.log(person.name+ " " + person.number));
+//         mongoose.connection.close();
+//     })
+    
+// } else if (process.argv.length === 5) {
+
+//     Person.find({name :process.argv[3]}).then(result => {
+//         if (result.length !== 0) {
+//             console.log('This name is already in the phonebook');
+//             mongoose.connection.close();
+//             return
+//         } else {
+//             let person = new Person({
+//                 name: process.argv[3],
+//                 number: process.argv[4]
+//             })
+//             person.save().then(result => {
+//                 console.log(`added ${process.argv[3]} ${process.argv[4]} to phonebook`);
+//                 mongoose.connection.close();
+//             })
+//         } 
+//     })
+// } else {
+//     console.log("Please provide: password (to list) or password name number (to add)");
+//     mongoose.connection.close();
+// }
+
+
+// const person = new Person ({
+//     name:"alan ture",
+//     number: "23-23289342"
+// })
+
+// person.save().then(result => {
+//     console.log('phone number saved!');
+//     mongoose.connection.close()
+// })
