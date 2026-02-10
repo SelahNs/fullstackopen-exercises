de8bd1c106521c9f79e16edec78d101854bd29be
Selehadin Nasir <nasir.selehadin@gmail.com>
2026-02-10 14:56:56 +0300

    feat: added ful functionality to the database

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -14,39 +14,45 @@ morgan.token('body', (request) => JSON.stringify(request.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (request, response, next) => {
     Person.find({}).then(result => {
         response.json(result);
     })
+    .catch(error => next(error))
 });
 
-app.get("/info", (request, response) => {
+app.get("/info", (request, response, next) => {
     Person.find({}).then(result => {
         const time = new Date();
         const num = result.length;
         response.send(`<div><p>Phonebook has info for ${num} people</p><p>${time}</p></div>`);
     })
+    .catch(error => next(error))
    
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(result => {
         response.json(result);
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id).then(result => {
         response.status(204).end();
     })
+    .catch(error => next(error))
     
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body;
+    
     if (!(body.name && body.number)) {
         return response.status(400).json({error: "content missing"})
     }
+
     // const check = persons.filter(person => person.name.toLowerCase() === body.name.toLowerCase());
 
     // if (check.length > 0) {
@@ -59,9 +65,32 @@ app.post('/api/persons', (request, response) => {
     person.save().then(result => {
         response.json(person)
     })    
+    .catch(error => next(error))
 
 })
 
+app.put('/api/persons/:id', (request, response, next)=> {
+    const {name, number} = request.body;
+    Person.findById(request.params.id).then(note => {
+        note.name = name;
+        note.number = number;
+
+        note.save().then(updatedNote => response.json(updatedNote))
+        .catch(error => next(error))
+
+    })
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return response.status(404).send({error: "misformated id"})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
