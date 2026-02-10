f14f5f29a0ef6e8c5ccf67a63b5b323035759a9a
Selehadin Nasir <nasir.selehadin@gmail.com>
2026-02-10 11:01:13 +0300

    feat: small bug fix and error handling

--- notes/backend/index.js
+++ notes/backend/index.js
@@ -14,27 +14,28 @@ app.get('/', (request, reponse) => {
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
-    response.json(notes);
+    response.json(notes); 
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+    if (note) {
+      response.json(note);
+    } else {
+      response.status(404).end()
+    }
+  }).catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id).then(result => {
   response.status(204).end();
   })
-  .catch(error => {
-    console.log(error)
-    response.status(400).send({ error: 'malfomatted id'})
-  })
+  .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response,next) => {
   const body = request.body;
   if (!body.content) {
     return response.status(400).json({error : "content missing"});
@@ -48,8 +49,42 @@ app.post('/api/notes', (request, response) => {
   note.save().then(result => {
     response.json(result)
   })
+  .catch(error => next(error))
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const {content, important} = request.body;
+  Note.findById(request.params.id).then(note => {
+    if (!note) {
+      return response.status(404).end()
+    } 
+    note.content = content;
+    note.important= important;
+
+    return note.save().then((updatedNote) => {
+      response.json(updatedNote)
+    })
+  })
+  .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
