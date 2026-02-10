8dd61239da3100de4d8a2365d9760852d806a5c1
Selehadin Nasir <nasir.selehadin@gmail.com>
2026-02-10 15:16:06 +0300

    feat: added validation

--- notes/backend/index.js
+++ notes/backend/index.js
@@ -79,6 +79,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({error: 'malformatted id'})
+  } else if(error.name === 'validationError') {
+    return response.status(400).json({error: error.message})
   }
 
   next(error)
