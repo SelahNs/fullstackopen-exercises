e91c7f09b62ac34257de1d6fc97787a1301dd603
Selehadin Nasir <nasir.selehadin@gmail.com>
2026-02-13 09:10:04 +0300

    feat : updated to good code structure

--- notes/backend/models/note.js
+++ notes/backend/models/note.js
@@ -1,18 +1,4 @@
 const mongoose = require('mongoose')
-// if (process.argv.length < 3) {
-//     console.log('Give pasword as an argument');
-//     process.exit(1);
-// }
-
-const url = process.env.MONGODB_URI
-
-mongoose.set('strictQuery', false)
-
-mongoose.connect(url,{ family: 4 }).then(result => {
-  console.log('connect to MongoDB')
-}).catch(error => {
-  console.group('error connecting to MongoDB:', error.message)
-})
 
 const noteSchema = new mongoose.Schema({
   content: {
