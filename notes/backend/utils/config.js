7e171d6cfbe281a339f3b87db627a8d61627e864
Selehadin Nasir <nasir.selehadin@gmail.com>
2026-02-19 11:37:44 +0300

    feat: structure the foder for testing using defferent data base

--- notes/backend/utils/config.js
+++ notes/backend/utils/config.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.NODE_ENV === 'test' 
+  ? process.env.NODE_ENV 
+  : process.env.MONGODB_URI;
 
 module.exports = {PORT, MONGODB_URI}
