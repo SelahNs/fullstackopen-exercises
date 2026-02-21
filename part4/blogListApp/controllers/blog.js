155fb754f4896c41fd9d49e3ce99a054f8996169
Selehadin Nasir <nasir.selehadin@gmail.com>
2026-02-21 13:06:32 +0300

    feat: refactored the post request to async/awiat

--- part4/blogListApp/controllers/blog.js
+++ part4/blogListApp/controllers/blog.js
@@ -7,13 +7,11 @@ blogRouter.get('/', async (request, response,next) => {
   response.json(blogs)
 })
 
-blogRouter.post('/', (request, response, next) => {
+blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-  .catch(error => next(error))
+  const result = await blog.save()
+  response.status(201).json(result)
 })
 
 
