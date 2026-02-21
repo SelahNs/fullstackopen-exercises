79bef40ae15c790e0509ce5dae03da49b9a40364
Selehadin Nasir <nasir.selehadin@gmail.com>
2026-02-21 11:27:37 +0300

    feat: refactord the delte to async await

--- notes/backend/controllers/notes.js
+++ notes/backend/controllers/notes.js
@@ -8,7 +8,7 @@ notesRouter.get('/', async (request, response) => {
 
 notesRouter.get('/:id', async (request, response) => {
   const note = await Note.findById(request.params.id)
-  
+
   if (note) {
     response.json(note)
   } else {
@@ -29,12 +29,9 @@ notesRouter.post('/', async (request, response, next) => {
   response.status(201).json(savedNote)
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndDelete(request.params.id);
+  response.status(204).end()
 })
 
 notesRouter.put('/:id', (request, response, next) => {
