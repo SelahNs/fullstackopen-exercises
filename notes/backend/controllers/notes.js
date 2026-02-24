07fd26d2578718b6e21be18fbc70318bf3c089e4
Selehadin Nasir <nasir.selehadin@gmail.com>
2026-02-24 11:05:38 +0300

    feat: added fulllogin in the backend with token authentication

--- notes/backend/controllers/notes.js
+++ notes/backend/controllers/notes.js
@@ -1,6 +1,16 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ','')
+  }
+
+  return null
+}
 
 notesRouter.get('/', async (request, response) => {
   
@@ -8,8 +18,13 @@ notesRouter.get('/', async (request, response) => {
     response.json(note)
 })
 
-notesRouter.get('/:id', async (request, response) => {
+notesRouter.get('/:id', async (response, request) => {
   const note = await Note.findById(request.params.id)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid'})
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (note) {
     response.json(note)
