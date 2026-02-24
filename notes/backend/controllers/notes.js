8e0b49f8293bad64ef509869f0cfc64e17192774
Selehadin Nasir <nasir.selehadin@gmail.com>
2026-02-24 10:06:15 +0300

    feat: added user login system

--- notes/backend/controllers/notes.js
+++ notes/backend/controllers/notes.js
@@ -1,8 +1,10 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-  const note = await Note.find({})
+  
+  const note = await Note.find({}).populate('user', { username: 1, name: 1})
     response.json(note)
 })
 
@@ -20,12 +22,21 @@ notesRouter.get('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
+  if (!user) {
+    return response.status(400).json({error: 'userId missing or not valid'})
+  }
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
   response.status(201).json(savedNote)
 })
 
